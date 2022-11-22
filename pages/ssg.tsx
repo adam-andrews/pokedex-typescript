0418d1a280836cc2365e422e07143875da4417eb
Adam Andrews <andrewsadam07@gmail.com>
2022-11-22 21:09:31 +0000

    added ssr and ssg

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Pokemon from '../components/pokemon';
+type pokemonProps = {
+	pokemon: pokemon[];
+};
 
-function ssg() {
-	return <div>ssg</div>;
+type pokemon = {
+	name: string;
+	url: string;
+};
+function ssg({ pokemon }: pokemonProps) {
+	console.log(pokemon, 'pokemon');
+	return (
+		<div>
+			{pokemon.map((pokemon) => {
+				return (
+					<Pokemon key={pokemon.name} name={pokemon.name} url={pokemon.url} />
+				);
+			})}
+		</div>
+	);
+}
+
+export async function getStaticProps() {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+	const { results } = await response.json();
+	results.map((pokemon: pokemon, index: number) => {
+		const pokiId = index + 1;
+		pokemon.url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokiId}.png`;
+	});
+
+	return {
+		props: {
+			pokemon: results,
+		},
+	};
 }
 
 export default ssg;
